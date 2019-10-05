d3c05a2fbd45ee98e7ed574afee157c0f5190de2
Lama-Albayomi <43874780+Lama-Albayomi@users.noreply.github.com>
2019-10-05 15:51:15 +0300

    PlayerMovement

---
+++ JustAnotherDay/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] Dialogues;
+    private int Index;
+    public float TypingSpeed;
+    public bool Continue;
+
+    public bool Active = false;
+
+
+    //Activate on Closure
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (Active == false)
+                {
+                    Active = true;
+                    StartCoroutine(Type());
+                }
+                if (Active)
+                {
+                    NextSentence();
+                }
+            }
+        }
+    }
+
+    //Enabling Con Button
+    void Update()
+    {
+        if (textDisplay.text == Dialogues[Index])
+        {
+            Continue = true;
+        }
+    }
+
+    //Main Typing Fuction
+    IEnumerator Type()
+    {
+        foreach (char letter in Dialogues[Index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(TypingSpeed);
+        }
+    }
+    //Continue Button Function
+
+    public void NextSentence()
+    {
+        if (Continue == true)
+        {
+            if (Index < Dialogues.Length - 1)
+            {
+                Index++;
+                textDisplay.text = "";
+                StartCoroutine(Type());
+                Continue = false;
+
+            }
+
+
+            else
+            {
+                textDisplay.text = "";
+                Continue = false;
+                Active = false;
+                Index = 0;
+
+            }
+        }
+    }
+}
---
+++ JustAnotherDay/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D body;
+    Animator animator;
+    SpriteRenderer PlayerSpriteRenderer;
+
+    // Directions 
+    int up = 0;
+    int Down = 1;
+    int Right = 2;
+    int Left = 3;
+
+    public float Speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        //animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) // Move Up
+        {
+            // Player Animation 
+            //animator.SetInteger("",up);
+
+            // Movement
+            body.MovePosition(transform.position + transform.up * Time.fixedDeltaTime * Speed);
+        }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) // Move DOWN
+        {
+            // Player Animation 
+            //animator.SetInteger("", Down);
+
+            // Movement
+            body.MovePosition(transform.position - transform.up * Time.fixedDeltaTime * Speed);
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) // Move RIGHT
+        {
+            // Player Animation 
+            //animator.SetInteger("", Right);
+
+            // Movement
+            body.MovePosition(transform.position + transform.right * Time.fixedDeltaTime * Speed);
+        }
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) // Move LEFT
+        {
+            // Player Animation 
+            //animator.SetInteger("", Left);
+
+            // Movement
+            body.MovePosition(transform.position - transform.right * Time.fixedDeltaTime * Speed);
+        }
+    }
+}
